1367bb5bd20562a265394cfe9939362362f352fa
Stheven Cabral <stheven.cabral@gmail.com>
2021-02-24 11:05:44 -0800

    Update styling to look more like overview reports.

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
 /***
- * @array dataColors - Array of colors for donut chart.
+ * @array dataColorsBlue - Array of colors for donut chart.
  * @array couponBreakdownChartData - Array of objects containing data for donut highchart.
  */
-const dataColors = ["#00366d", "#114b82", "#286097", "#3c76ae", "#538bc2", "#95c1ec", "#bfdbf8"];
+const dataColorsBlue = ["#0E003C", "#1c1880", "#1d2d96", "#1f4cab", "#1f4cab", "#308bc4", "#3dabcb", "#3dabcb", "#7ddcdc", "#7ddcdc"];
+const dataColorsGreen = ["#081F1C", "#125131", "#167633", "#21A235", "#36BA39", "#54C53C", "#78CB42", "#9CD353", "#C2E576", "#E7F4B8"];
 
 const couponBreakdownChartData = [
   {
     y: 51.7,
     name: "0 %",
-    color: dataColors[0]
+    color: dataColorsGreen[0]
   },
   {
     y: 8.28,
     name: "0-2 %",
-    color: dataColors[1]
+    color: dataColorsGreen[1]
   },
   {
     y: 32.79,
     name: "2-4 %",
-    color: dataColors[2]
+    color: dataColorsGreen[2]
   },
   {
     y: 5.85,
     name: "4-6 %",
-    color: dataColors[3]
+    color: dataColorsGreen[3]
   },
   {
     y: 1.37,
     name: "6-8 %",
-    color: dataColors[4]
+    color: dataColorsGreen[4]
   },
   {
     y: 0.01,
     name: "8-10 %",
-    color: dataColors[5]
+    color: dataColorsGreen[5]
   },
   {
     y: 0,
     name: ">10 %",
-    color: dataColors[6]
+    color: dataColorsGreen[6]
   },
 ];
 
@@ -53,8 +54,8 @@ document.addEventListener('DOMContentLoaded', function () {
       plotBackgroundColor: null,
       plotBorderWidth: 0,
       plotShadow: false,
-      width: 350,
-      height: 350,
+      width: 355,
+      height: 355,
       margin: [0, 0, 0, 0]
     },
     title: {
@@ -101,10 +102,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
   /***
    * On document load, the circle icon colors in the 'Coupon Breakdown' table are assigned
-   based on the 'dataColors' array.
+   based on the 'dataColorsGreen' array.
    */
   const htmlElement = document.querySelector("html");
-  for(let i=0; i <= dataColors.length; i++) {
-    htmlElement.style.setProperty(`--category-circle-icon-${i}`, dataColors[i]);
+  for (let i = 0; i <= dataColorsGreen.length; i++) {
+    htmlElement.style.setProperty(`--category-circle-icon-${i}`, dataColorsGreen[i]);
   }
 });
